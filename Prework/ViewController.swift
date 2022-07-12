5c4cd29aa4fd44106f0d953accde015284eb2bf5
Nick Melkadze <25963683+melkadze@users.noreply.github.com>
2022-07-12 12:29:13 -0400

    add extra features plus an about page

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -5,6 +5,11 @@
 //  Created by Nick Melkadze on 7/11/22.
 //
 
+/// What has been implemented already:
+/// Normal tip function
+/// Setitngs menu
+///
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,10 +17,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var splitControl: UIStepper!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var splitTotalCostLabel: UILabel!
+    @IBOutlet weak var splitTotalInfoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // set title in Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // set first responder
+        billAmountTextField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // set theme
+        switch UserDefaults.standard.integer(forKey: "theme") {
+        case 0:
+            overrideUserInterfaceStyle = .light
+        case 2:
+            overrideUserInterfaceStyle = .dark
+        default: // case 1, or any other (erraneous) value
+            overrideUserInterfaceStyle = .unspecified
+        }
+        
+        // set slider visibility
+        if (UserDefaults.standard.bool(forKey: "slider")) {
+            tipControl.isHidden = true
+            tipLabel.isHidden = false
+            tipSlider.isHidden = false
+            updateSlider(UIButton.self)
+        } else {
+            tipControl.isHidden = false
+            tipLabel.isHidden = true
+            tipSlider.isHidden = true
+            calculateTip(UIButton.self)
+        }
+        
+        // set splitting visibility
+        if (UserDefaults.standard.bool(forKey: "splitting")) {
+            splitControl.isHidden = false
+            splitNumberLabel.isHidden = false
+            splitTotalInfoLabel.isHidden = false
+            splitTotalCostLabel.isHidden = false
+        } else {
+            splitControl.isHidden = true
+            splitNumberLabel.isHidden = true
+            splitTotalInfoLabel.isHidden = true
+            splitTotalCostLabel.isHidden = true
+        }
     }
     
     @IBAction func calculateTip(_ sender: Any) {
@@ -33,8 +90,45 @@ class ViewController: UIViewController {
         let totalBill = initialBill + tip
         
         // Step 3 (update labels):
+        let currency = UserDefaults.standard.string(forKey: "currency") ?? "$"
         // we use %.2f to print 2 decimal places
-        tipAmountLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", totalBill)
+        tipAmountLabel.text = String(format: currency + "%.2f", tip)
+        totalLabel.text = String(format: currency + "%.2f", totalBill)
+        
+        if (UserDefaults.standard.bool(forKey: "splitting")) {
+            splitTotalCostLabel.text = String(format: currency + "%.2f", totalBill / splitControl.value)
+        }
+    }
+    
+    @IBAction func updateSlider(_ sender: Any) {
+        tipLabel.text = String(Int(tipSlider.value)) + "%"
+        
+        let initialBill = Float(billAmountTextField.text!) ?? 0 // if the value does not exist, this uses 0
+        let tip = initialBill * round(tipSlider.value) / 100
+        let totalBill = initialBill + tip
+        
+        // Step 3 (update labels):
+        let currency = UserDefaults.standard.string(forKey: "currency") ?? "$"
+        // we use %.2f to print 2 decimal places
+        tipAmountLabel.text = String(format: currency + "%.2f", tip)
+        totalLabel.text = String(format: currency + "%.2f", totalBill)
+        
+        if (UserDefaults.standard.bool(forKey: "splitting")) {
+            splitTotalCostLabel.text = String(format: currency + "%.2f", totalBill / Float(splitControl.value))
+        }
+    }
+    
+    @IBAction func billAmountChanged(_ sender: Any) {
+        UserDefaults.standard.bool(forKey: "slider") ? updateSlider(UIButton.self) : calculateTip(UIButton.self)
+    }
+    
+    @IBAction func splitControlUpdated(_ sender: Any) {
+        splitNumberLabel.text = "Split bill between " + String(Int(splitControl.value)) + (splitControl.value == 1 ? " person" : " people")
+        
+        if (UserDefaults.standard.bool(forKey: "slider")) {
+            updateSlider(UIButton.self)
+        } else {
+            calculateTip(UIButton.self)
+        }
     }
 }
---
+++ SettingsViewController.swift
@@ -0,0 +1,129 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Nick Melkadze on 7/11/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var themeControl: UISegmentedControl!
+    @IBOutlet weak var currencyButton: UIButton!
+    @IBOutlet weak var sliderControl: UISwitch!
+    @IBOutlet weak var splittingControl: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // set nav title and setup the button default values
+        self.title = "Settings"
+        setupButtons()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presentingViewController?.viewWillDisappear(true)
+        
+        // set theme
+        switch UserDefaults.standard.integer(forKey: "theme") {
+        case 0:
+            overrideUserInterfaceStyle = .light
+        case 2:
+            overrideUserInterfaceStyle = .dark
+        default: // case 1, or any other (erraneous) value
+            overrideUserInterfaceStyle = .unspecified
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        presentingViewController?.viewWillAppear(true)
+    }
+    
+    func setupButtons() {
+        // currency button:
+        let currencyOptionClosure = {(action : UIAction) in self.saveCurrency(value: action.title)}
+        
+        let currency = UserDefaults.standard.string(forKey: "currency")
+        
+        currencyButton.menu = UIMenu(children : [
+            UIAction(title : "$", state : (currency == "$" ? .on : .off), handler: currencyOptionClosure),
+            UIAction(title : "£", state : (currency == "£" ? .on : .off), handler: currencyOptionClosure),
+            UIAction(title : "€", state : (currency == "€" ? .on : .off), handler: currencyOptionClosure),
+            UIAction(title : "₹", state : (currency == "₹" ? .on : .off), handler: currencyOptionClosure),
+            UIAction(title : "¥", state : (currency == "¥" ? .on : .off), handler: currencyOptionClosure)])
+        
+        currencyButton.showsMenuAsPrimaryAction = true
+        currencyButton.changesSelectionAsPrimaryAction = true
+        
+        
+        // theme button:
+        switch UserDefaults.standard.integer(forKey: "theme") {
+        case 0:
+            themeControl.selectedSegmentIndex = 0
+        case 2:
+            themeControl.selectedSegmentIndex = 2
+        default: // case 1, or any other (erraneous) value
+            themeControl.selectedSegmentIndex = 1
+        }
+        
+        // slider button:
+        if (UserDefaults.standard.bool(forKey: "slider")) {
+            sliderControl.isOn = true
+        } else {
+            sliderControl.isOn = false
+        }
+        
+        // splitting button:
+        if (UserDefaults.standard.bool(forKey: "splitting")) {
+            splittingControl.isOn = true
+        } else {
+            splittingControl.isOn = false
+        }
+    }
+    
+    func saveCurrency(value: String) {
+        UserDefaults.standard.set(value, forKey: "currency")
+        UserDefaults.standard.synchronize()
+    }
+    
+    var toggleVar = true
+    
+    @IBAction func setTheme(_ sender: UISegmentedControl) {
+        UserDefaults.standard.set(themeControl.selectedSegmentIndex, forKey: "theme")
+        UserDefaults.standard.synchronize()
+        
+        switch themeControl.selectedSegmentIndex {
+        case 0:
+            overrideUserInterfaceStyle = .light
+        case 2:
+            overrideUserInterfaceStyle = .dark
+        default: // case 1, or any other (erraneous) value
+            overrideUserInterfaceStyle = .unspecified
+        }
+    }
+    
+    @IBAction func sliderModeChanged(_ sender: UISwitch) {
+        UserDefaults.standard.set((sliderControl.isOn ? true : false), forKey: "slider")
+        UserDefaults.standard.synchronize()
+    }
+    
+    @IBAction func splittingModeChanged(_ sender: Any) {
+        UserDefaults.standard.set((splittingControl.isOn ? true : false), forKey: "splitting")
+        UserDefaults.standard.synchronize()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
